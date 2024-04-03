4e9a662ab148fc5bf27aeafb0c2fc6442aa72ac2
Brian Omondi <brianhilsden@gmail.com>
2024-04-03 12:06:59 +0300

    mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.forms[0];
+  let new_task = form.elements.new_task_description;
+  const tasksList = document.getElementById("tasks");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const task = document.createElement("li");
+    task.textContent = new_task.value;
+    tasksList.appendChild(task);
+  });
 });
