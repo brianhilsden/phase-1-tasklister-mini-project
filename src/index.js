4af59f13130236041e934eec5b34166f68652bd9
Brian Omondi <brianhilsden@gmail.com>
2024-04-03 15:34:31 +0300

    added delete functionality

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
   let form = document.forms[0];
   let new_task = form.elements.new_task_description;
   const tasksList = document.getElementById("tasks");
+
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const task = document.createElement("li");
     task.textContent = new_task.value;
     tasksList.appendChild(task);
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "delete";
+    task.append(deleteButton);
+    deleteButton.addEventListener("click", () => {
+      task.remove();
+    });
   });
 });
